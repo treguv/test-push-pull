f7617801652a627f8b10e38d3880a2e2f1549e80
lammai <67616507+lammai@users.noreply.github.com>
2020-10-16 17:06:02 -0700

    updated HelloWorld.java

--- default-package/src/HelloWorld.java
+++ default-package/src/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
     public static void main(String[] theArgs) {
         System.out.println("Hello World!");
         System.out.print("Vlad Pushed!");
+        System.out.println("Lam Pushed!");
     }
 }
