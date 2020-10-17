ff4c8cc31c1c8e8c40f3855932ef85dfbd460789
treguv <vladislavtregubov00@gmail.com>
2020-10-16 17:01:09 -0700

    Updated hello world

--- default-package/src/HelloWorld.java
+++ default-package/src/HelloWorld.java
@@ -4,6 +4,7 @@
 public class HelloWorld {
     // hello world main
     public static void main(String[] theArgs) {
-        System.out.println("Hello");
+        System.out.println("Hello World!");
+        System.out.print("Vlad Pushed!");
     }
 }
